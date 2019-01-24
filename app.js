1abb174a410915ab26d7f57dd59f6b62cfff1bb3
Kranmas <njkranmas@labs.neit.edu>
2019-01-24 15:06:44 -0500

    added game list app

---
+++ app.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var app = express();
+var port = 5000;
+var path = require('path');
+var router = express.Router();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+
+mongoose.Promise = global.Promise;
+
+//connect to mongodb using mongoose 
+mongoose.connect("mongodb://localhost:27017/gameEntries", {
+    useMongoClient:true
+}).then(function(){
+    console.log("Connected to the Monogo Database")
+}).catch(function(err){
+    console.log(err);
+});
+
+require('./models/Entry');
+var Entry = mongoose.model('Entries');
+// functions to use body parser 
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+//route to index.html
+router.get('/',function(req, res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.get('/getdata', function(req,res){
+    console.log("Request made from fetch");
+    Entry.find({}).then(function(entries){
+        res.send({
+            entries:entries
+        });
+    });
+});
+
+//route to entries.html
+router.get('/entries',function(req, res){
+    res.sendFile(path.join(__dirname+'/entries.html'));
+});
+
+//post for form on index.html
+app.post('/', function(req,res){
+    console.log(req.body);
+    var newEntry = {
+        title:req.body.title,
+        genre:req.body.genre
+    }
+
+    new Entry(newEntry).save().then(function(entry){
+        res.redirect('/');
+    });
+});
+
+//routs for paths
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/scripts'))
+app.use('/', router);
+
+//starts the server 
+app.listen(port, function(){
+    console.log("server is running on port: " + port);
+});
---
+++ models/Entry.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var EntrySchema = new Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    genre:{
+        type:String,
+        required:true
+    },
+    date:{
+        type:Date,
+        default:Date.now
+    }
+});
+
+mongoose.model('Entries', EntrySchema);
